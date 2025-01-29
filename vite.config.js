fcd9376b350c35d730e31031a4392c6ec609df15
unknown <dviezmc@gmail.com>
2025-01-29 02:11:27 -0500

    Updated deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/uncjae-hotel/', // Actualizado para coincidir con tu repositorio
+  base: '/uncjae-hotel/', // Este debe coincidir con el nombre de tu repositorio
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
